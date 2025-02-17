5a0df3d675a4b44363352193afa3b9a227ca0c80
Jesse Otim <otimdanieljesse@gmail.com>
2025-02-17 12:38:01 +0300

    Added nationality dropdown and refactor code

--- src/layouts/OnboardingPages/CitizenshipPage.tsx
+++ src/layouts/OnboardingPages/CitizenshipPage.tsx
@@ -1,68 +1,63 @@
-import { Form, Radio, Select, Input } from 'antd';
+import { Form, Select, Input } from 'antd';
 
 const CitizenshipPage = ({ formData, setFormData }) => {
     const [form] = Form.useForm();
 
     return (
-        <Form
-            form={form}
-            layout="vertical"
-            initialValues={formData.citizenshipData || {}}
-            onValuesChange={(_, allValues) => {
-                setFormData((prev) => ({ ...prev, citizenshipData: allValues }));
-            }}
-        >
-            <Form.Item
-                name="citizenshipStatus"
-                label="Are You a Ugandan Citizen or a Refugee?"
-                rules={[{ required: true, message: 'Please select your citizenship status' }]}
+        <div>
+            <h1 className="text-xl/8 font-semibold text-gray-900 sm:text-lg/9 mb-6">
+                Are you a citizen or Refugee?
+            </h1>
+            <Form
+                form={form}
+                layout="vertical"
+                initialValues={formData.citizenshipData || {}}
+                onValuesChange={(_, allValues) => {
+                    setFormData((prev) => ({ ...prev, citizenshipData: allValues }));
+                }}
             >
-                <Radio.Group>
-                    <Radio value="ugandan">Ugandan Citizen</Radio>
-                    <Radio value="refugee">Refugee</Radio>
-                </Radio.Group>
-            </Form.Item>
+                <Form.Item
+                    name="nationality"
+                    label="Nationality"
+                    rules={[{ required: true, message: 'Please select your nationality' }]}
+                    className="my-24"
+                >
+                    <Select placeholder="Select your nationality" size="large">
+                        <Select.Option value="ugandan">National/Ugandan</Select.Option>
+                        <Select.Option value="south_sudan">Refugee - South Sudan</Select.Option>
+                        <Select.Option value="congolese">Refugee - Congolese</Select.Option>
+                        <Select.Option value="rwandese">Refugee - Rwandese</Select.Option>
+                        <Select.Option value="burundian">Refugee - Burundian</Select.Option>
+                        <Select.Option value="somali">Refugee - Somali</Select.Option>
+                        <Select.Option value="sudanese">Refugee - Sudanese</Select.Option>
+                        <Select.Option value="others">Refugee - Others</Select.Option>
+                    </Select>
+                </Form.Item>
 
-            {form.getFieldValue('citizenshipStatus') === 'refugee' && (
-                <>
+                {form.getFieldValue('nationality') === 'ugandan' && (
                     <Form.Item
-                        name="nationalityCategory"
-                        label="Nationality Category"
-                        rules={[{ required: true, message: 'Please select your nationality category' }]}
+                        name="nin"
+                        label="National Identification Number (NIN)"
+                        rules={[{ required: true, message: 'Please enter your NIN' }]}
+                        className="my-24"
                     >
-                        <Select placeholder="Select your nationality">
-                            <Select.Option value="south_sudan">Refugee - South Sudan</Select.Option>
-                            <Select.Option value="congolese">Refugee - Congolese</Select.Option>
-                            <Select.Option value="rwandese">Refugee - Rwandese</Select.Option>
-                            <Select.Option value="burundian">Refugee - Burundian</Select.Option>
-                            <Select.Option value="somali">Refugee - Somali</Select.Option>
-                            <Select.Option value="sudanese">Refugee - Sudanese</Select.Option>
-                            <Select.Option value="others">Refugee - Others</Select.Option>
-                            <Select.Option value="ugandan">National/Ugandan</Select.Option>
-                        </Select>
+                        <Input size="large" placeholder="Enter your NIN" />
                     </Form.Item>
+                )}
 
+                {form.getFieldValue('nationality') !== 'ugandan' && (
                     <Form.Item
                         name="uniqueIdNo"
                         label="Unique Identification No."
                         rules={[{ required: true, message: 'Please enter your Unique Identification Number' }]}
+                        className="mb-6"
                     >
-                        <Input placeholder="Enter your Unique Identification Number" />
+                        <Input size="large" placeholder="Enter your Unique Identification Number" />
                     </Form.Item>
-                </>
-            )}
-
-            {form.getFieldValue('citizenshipStatus') === 'ugandan' && (
-                <Form.Item
-                    name="nin"
-                    label="National Identification Number (NIN)"
-                    rules={[{ required: true, message: 'Please enter your NIN' }]}
-                >
-                    <Input placeholder="Enter your NIN" />
-                </Form.Item>
-            )}
-        </Form>
+                )}
+            </Form>
+        </div>
     );
 };
 
-export default CitizenshipPage; 
+export default CitizenshipPage;
