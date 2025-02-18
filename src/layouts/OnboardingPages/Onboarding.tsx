8f31bb5aeeb19c8f8f99bd6c590aecdf53a550c6
Jesse Otim <otimdanieljesse@gmail.com>
2025-02-18 11:54:41 +0300

    Add responsiveness and refactor code

--- src/layouts/OnboardingPages/Onboarding.tsx
+++ src/layouts/OnboardingPages/Onboarding.tsx
@@ -1,159 +1,168 @@
-import React, { useState } from 'react';
-import { Button, Progress, Form } from 'antd';
-import WelcomePage from './WelcomePage';
-import InformationPage from './InformationPage';
-import RetiCandidatePage from './RetiCandidatePage';
-import AdditionalInformationPage from './AdditionalInformationPage';
-import CitizenshipPage from './CitizenshipPage';
+import React, { useState } from "react";
+import { Button, Progress, Form } from "antd";
+import WelcomePage from "./WelcomePage";
+import InformationPage from "./InformationPage";
+import RetiCandidatePage from "./RetiCandidatePage";
+import AdditionalInformationPage from "./AdditionalInformationPage";
+import CitizenshipPage from "./CitizenshipPage";
 import { useCreateProfileMutation } from "../../services/profiles.ts";
 import { userDetails } from "../../utils.ts";
-import OnboardSuccessPage from './OnboardSuccessPage';
-import moment from 'moment';
+import OnboardSuccessPage from "./OnboardSuccessPage";
+import moment from "moment";
 
 const Onboarding: React.FC = () => {
-    const [form] = Form.useForm();
-    const [current, setCurrent] = useState(0);
-    const [formData, setFormData] = useState<Record<string, any>>({});
-    const [submissionStatus, setSubmissionStatus] = useState<'idle' | 'success' | 'error'>('idle');
+  const [form] = Form.useForm();
+  const [current, setCurrent] = useState(0);
+  const [formData, setFormData] = useState<Record<string, any>>({});
+  const [submissionStatus, setSubmissionStatus] = useState<
+    "idle" | "success" | "error"
+  >("idle");
 
-    const next = () => setCurrent((prev) => prev + 1);
-    const prev = () => setCurrent((prev) => prev - 1);
+  const next = () => setCurrent((prev) => prev + 1);
+  const prev = () => setCurrent((prev) => prev - 1);
 
-    const steps = [
-        {
-            title: 'Welcome',
-            content: () => <WelcomePage />,
-            key: 'welcomeData',
-        },
-        {
-            title: 'Second',
-            content: () => <InformationPage setFormData={setFormData} />,
-            key: 'informationData',
-        },
-        {
-            title: 'Third',
-            content: () => <RetiCandidatePage formData={formData} setFormData={setFormData} />,
-            key: 'sectionsData',
-        },
-        {
-            title: 'Citizenship Status',
-            content: () => <CitizenshipPage formData={formData} setFormData={setFormData} />,
-            key: 'citizenshipData',
-        },
-        {
-            title: 'Additional Information',
-            content: () => <AdditionalInformationPage formData={formData} setFormData={setFormData} />,
-            key: 'lastData',
-        },
-    ];
-    const [updateUser] = useCreateProfileMutation()
-    const progressPercentage = ((current + 1) / steps.length) * 100;
+  const steps = [
+    {
+      title: "Welcome",
+      content: () => <WelcomePage />,
+      key: "welcomeData",
+    },
+    {
+      title: "Second",
+      content: () => <InformationPage setFormData={setFormData} />,
+      key: "informationData",
+    },
+    {
+      title: "Third",
+      content: () => (
+        <RetiCandidatePage formData={formData} setFormData={setFormData} />
+      ),
+      key: "sectionsData",
+    },
+    {
+      title: "Citizenship Status",
+      content: () => (
+        <CitizenshipPage formData={formData} setFormData={setFormData} />
+      ),
+      key: "citizenshipData",
+    },
+    {
+      title: "Additional Information",
+      content: () => (
+        <AdditionalInformationPage
+          formData={formData}
+          setFormData={setFormData}
+        />
+      ),
+      key: "lastData",
+    },
+  ];
+  const [updateUser] = useCreateProfileMutation();
+  const progressPercentage = ((current + 1) / steps.length) * 100;
 
-    const handleFormChange = (allValues: any) => {
-        setFormData(prev => ({
-            ...prev,
-            ...allValues,
-            sectionsData: {
-                ...prev.sectionsData,
-                ...allValues.sectionsData
-            },
-            lastData: {
-                ...prev.lastData,
-                ...allValues.lastData
-            }
-        }));
+  const handleFormChange = (allValues: any) => {
+    setFormData((prev) => ({
+      ...prev,
+      ...allValues,
+      sectionsData: {
+        ...prev.sectionsData,
+        ...allValues.sectionsData,
+      },
+      lastData: {
+        ...prev.lastData,
+        ...allValues.lastData,
+      },
+    }));
+  };
+
+  const handleFinish = async () => {
+    const finalValues = { ...formData, ...form.getFieldsValue() };
+    const { firstName, lastName, ...restData } = finalValues;
+    const age = moment().diff(finalValues.dateOfBirth, "years");
+
+    const profilePayload = {
+      ...restData,
+      age: age,
     };
 
-    const handleFinish = async () => {
-        const finalValues = { ...formData, ...form.getFieldsValue() };
-        const { firstName, lastName, ...restData } = finalValues;
-        const age = moment().diff(finalValues.dateOfBirth, 'years');
+    const response = await updateUser({
+      profile: profilePayload,
+      profileId: userDetails()?.user.id,
+    }).unwrap();
+    console.log(response);
+    setSubmissionStatus("success");
+    localStorage.removeItem("userDetails");
+  };
 
-        const profilePayload = {
-            ...restData,
-            age: age,
-        };
+  if (submissionStatus === "success") {
+    return <OnboardSuccessPage />;
+  }
 
-        const response = await updateUser({
-            profile: profilePayload,
-            profileId: userDetails()?.user.id
-        }).unwrap();
-        console.log(response);
-        setSubmissionStatus('success');
-        localStorage.removeItem('userDetails');
-    };
-
-    if (submissionStatus === 'success') {
-        return <OnboardSuccessPage />;
-    }
-
-    return (
-        <div className='py-20'>
-            <div className="mx-auto max-w-2xl">
-                <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-                    <Form
-                        form={form}
-                        onFinish={handleFinish}
-                        onValuesChange={handleFormChange}
-                        initialValues={formData}
-                    >
-                        {/* Progress bar & step text */}
-                        <div className='px-2'>
-                            <div className="font-semibold text-sm text-gray-900">
-                                Step {current + 1} of {steps.length}
-                            </div>
-                            <Progress percent={progressPercentage} showInfo={false} />
-                        </div>
-
-                        {/* Step content */}
-                        <div className="sm:h-[500px] px-2 w-full sm:overflow-hidden">
-
-                            {/* {steps[current].content} */}
-                            {steps[current].content()}
-                        </div>
-
-                        {/* Navigation buttons */}
-                        <div className="mt-10 text-right">
-                            {current > 0 && (
-                                <Button className="ml-2 w-24" onClick={() => prev()}>
-                                    Back
-                                </Button>
-                            )}
-                            {current < steps.length - 1 ? (
-                                <Button
-                                    className="ml-2 w-24"
-                                    type="primary"
-                                    onClick={() => {
-                                        form
-                                            .validateFields()
-                                            .then(() => {
-                                                setFormData((prev) => ({ ...prev, ...form.getFieldsValue() })); // Ensure all fields are saved
-                                                if (current < steps.length - 1) {
-                                                    next(); // Move to next step only if it's not the last
-                                                }
-                                            })
-                                            .catch((err) => console.log('Validation Failed:', err));
-                                    }}
-                                >
-                                    Next
-                                </Button>
-
-
-                            ) : (
-                                <Button
-                                    className="ml-2 w-24"
-                                    type="primary"
-                                    htmlType="submit"
-                                >
-                                    Finish
-                                </Button>
-                            )}
-                        </div>
-                    </Form>
-                </div>
+  return (
+    <div>
+      <div className="mx-auto max-w-2xl">
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <Form
+            form={form}
+            onFinish={handleFinish}
+            onValuesChange={handleFormChange}
+            initialValues={formData}
+          >
+            {/* Progress bar & step text */}
+            <div className="px-2">
+              <div className="font-semibold text-sm text-gray-900">
+                Step {current + 1} of {steps.length}
+              </div>
+              <Progress percent={progressPercentage} showInfo={false} />
             </div>
+
+            {/* Step content */}
+            <div className="sm:h-[500px] px-2 w-full sm:overflow-hidden">
+              {/* {steps[current].content} */}
+              {steps[current].content()}
+            </div>
+
+            {/* Navigation buttons */}
+            <div className="fixed sm:static bottom-2 left-0 right-0 sm:right-auto sm:bottom-auto sm:ml-auto sm:mr-0 sm:mt-4">
+              <div className="flex justify-end gap-2 max-w-2xl mx-auto sm:mx-0 sm:max-w-none px-4 sm:px-0">
+                {current > 0 && (
+                  <Button className="w-24" onClick={() => prev()}>
+                    Back
+                  </Button>
+                )}
+                {current < steps.length - 1 ? (
+                  <Button
+                    className="w-24"
+                    type="primary"
+                    onClick={() => {
+                      form
+                        .validateFields()
+                        .then(() => {
+                          setFormData((prev) => ({
+                            ...prev,
+                            ...form.getFieldsValue(),
+                          }));
+                          if (current < steps.length - 1) {
+                            next();
+                          }
+                        })
+                        .catch((err) => console.log("Validation Failed:", err));
+                    }}
+                  >
+                    Next
+                  </Button>
+                ) : (
+                  <Button className="w-24" type="primary" htmlType="submit">
+                    Finish
+                  </Button>
+                )}
+              </div>
+            </div>
+          </Form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Onboarding;
