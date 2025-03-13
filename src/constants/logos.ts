2c59b3310b4ab8b517f00def85b7a91d1b04044c
Nta1e <ericebuluochol@gmail.com>
2025-03-13 21:19:54 +0300

    proper path for yarid

--- src/constants/logos.ts
+++ src/constants/logos.ts
@@ -5,7 +5,7 @@ const logos = [
     new URL("/src/assets/logos/ceford.png", import.meta.url).href,
     new URL("/src/assets/logos/dca.png", import.meta.url).href,
     new URL("/src/assets/logos/fca.png", import.meta.url).href,
-    new URL("/src/assets/logos/Yarid.png", import.meta.url).href,
+    new URL("/src/assets/logos/Yarid .png", import.meta.url).href,
     new URL("/src/assets/logos/guluuniversity.png", import.meta.url).href,
     new URL("/src/assets/logos/palmcorps.png", import.meta.url).href,
     new URL("/src/assets/logos/muni.png", import.meta.url).href,
